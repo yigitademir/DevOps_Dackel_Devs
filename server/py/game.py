a242cbfe7a2fd75d57418a7260f7f285ac446ef0
Oliver Staubli <oliver.staubli@revolytics.com>
2024-11-14 10:19:36 +0100

    game tempalte simpified, aligned with wiki

--- server/py/game.py
+++ server/py/game.py
@@ -1,46 +1,46 @@
-from typing import List, Any, Dict
-import abc
+from typing import List, Any
+from abc import ABCMeta, abstractmethod
 
 GameState = Any
 GameAction = Any
 
 
-class Game(metaclass=abc.ABCMeta):
+class Game(metaclass=ABCMeta):
 
-    @abc.abstractmethod
+    @abstractmethod
     def set_state(self, state: GameState) -> None:
         """ Set the game to a given state """
         pass
 
-    @abc.abstractmethod
+    @abstractmethod
     def get_state(self) -> GameState:
         """ Get the complete, unmasked game state """
         pass
 
-    @abc.abstractmethod
+    @abstractmethod
     def print_state(self) -> None:
         """ Print the current game state """
         pass
 
-    @abc.abstractmethod
+    @abstractmethod
     def get_list_action(self) -> List[GameAction]:
         """ Get a list of possible actions for the active player """
         pass
 
-    @abc.abstractmethod
+    @abstractmethod
     def apply_action(self, action: GameAction) -> None:
         """ Apply the given action to the game """
         pass
 
-    @abc.abstractmethod
+    @abstractmethod
     def get_player_view(self, idx_player: int) -> GameState:
         """ Get the masked state for the active player (e.g. the oppontent's cards are face down)"""
         pass
 
 
-class Player(metaclass=abc.ABCMeta):
+class Player(metaclass=ABCMeta):
 
-    @abc.abstractmethod
+    @abstractmethod
     def select_action(self, state: GameState, actions: List[GameAction]) -> GameAction:
         """ Given masked game state and possible actions, select the next action """
         pass
