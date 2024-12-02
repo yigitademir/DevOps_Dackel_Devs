ce4a614ae653b7b08a154a72d344fa0b50cf8b75
Kasper Kuznik <kasperr14@icloud.com>
2024-12-02 08:49:13 +0100

    Addition of dog instance attributes

--- server/py/dog.py
+++ server/py/dog.py
@@ -104,8 +104,9 @@ class Dog(Game):
     }
 
     def __init__(self) -> None:
-        """ Game initialization (set_state call not necessary, we expect 4 players) """
-        pass
+        self.state = GameState()
+        self.state.phase = GamePhase.SETUP
+        print(f"Game initialized. Phase: {self.state.phase}")
 
     def set_state(self, state: GameState) -> None:
         """ Set the game to a given state """
