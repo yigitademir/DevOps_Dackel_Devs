d3fa16f44aba28a18eeae0bd6bb704c1d6b36a7c
Kasper Kuznik <kasperr14@icloud.com>
2024-12-02 11:15:26 +0100

    Changes into set_state & get_state

--- server/py/dog.py
+++ server/py/dog.py
@@ -162,12 +162,10 @@ class Dog(Game):
         self.state.list_id_card_draw = self.state.LIST_CARD.copy() # Set draw pile
 
     def set_state(self, state: GameState) -> None:
-        """ Set the game to a given state """
-        pass
+        self.state = state
 
     def get_state(self) -> GameState:
-        """ Get the complete, unmasked game state """
-        pass
+        return self.state
 
     def print_state(self) -> None:
         """ Print the current game state """
